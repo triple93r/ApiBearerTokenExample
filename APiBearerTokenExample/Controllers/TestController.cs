9f36254c8ca3b712327c640f2b260f0c7e751e26
triple93r <56282248+triple93r@users.noreply.github.com>
2023-09-27 08:52:11 +0530

    init
    APi bearer token

---
+++ APiBearerTokenExample/Controllers/TestController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APiBearerTokenExample.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : Controller
+    {
+        public TestController()
+        {
+                
+        }
+
+        string Url = "";
+
+        public IActionResult logincheck()
+        {
+            string userName = "kapil", passwod = "kapil@12";
+
+            var keyvalues = new List<KeyValuePair<string, string>>
+            {
+                new KeyValuePair<string, string>("username", userName),
+                new KeyValuePair<string, string>("passwrd", passwod),
+                new KeyValuePair<string, string>("grant_type", "password")
+            };
+            //var KeyValuePair<string, string>
+
+            var requst = new HttpRequestMessage(HttpMethod.Post, Url + "Token");
+
+            return Ok();
+        }
+
+        
+    }
+}
